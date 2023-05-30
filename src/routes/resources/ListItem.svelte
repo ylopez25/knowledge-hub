25f4c971c24f4fe3cfce1641bce45a9d1f84f629
Yesenia L <ylopezcr123@gmail.com>
2023-05-30 16:28:34 -0400

    normalized title length

--- src/routes/resources/ListItem.svelte
+++ src/routes/resources/ListItem.svelte
@@ -31,11 +31,11 @@
         <span>
           {resource.title}
         </span>
-        <span class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
+        <!-- <span class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" height="24" width="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline bg-center">
             <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
           </svg>
-        </span>
+        </span> -->
       </div>
       <p class="mt-2 text-gray-700 grow">{resource.description}</p>
       <div class="flex flex-wrap text-xs py-2">
